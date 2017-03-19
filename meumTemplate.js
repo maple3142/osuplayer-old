bd2fa39727d8700cdc0e0f039d499cad967997aa
maple3142 <kirby741852963@gmail.com>
2017-03-19 12:14:31 +0800

    update

--- meumTemplate.js
+++ meumTemplate.js
@@ -14,6 +14,7 @@ module.exports=(g)=>{
                     },
                     {
                         label: 'exit',
+                        role: 'close',
                         click(){g.process.exit()}
                     }
                 ]
--- run.bat
+++ run.bat
@@ -1,2 +1,1 @@
-@echo off
-.\node_modules\.bin\electron .
+@.\node_modules\.bin\electron .
