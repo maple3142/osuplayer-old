ada230908ecd55f08e6dbf43f0aa8e7ddfdaa430
maple3142 <kirby741852963@gmail.com>
2017-03-19 10:51:43 +0800

    init

---
+++ meumTemplate.js
@@ -0,0 +1,43 @@
+module.exports=(g)=>{
+    return [
+            {
+                label: 'Action',
+                submenu:[
+                    {
+                        label: 'reload',
+                        role: 'reload',
+                        click(){g.win.webContents.executeJavaScript('reload()');}
+                    },
+                    {
+                        label: 'randomPlay',
+                        click(){g.win.webContents.executeJavaScript('randomPlay()');}
+                    },
+                    {
+                        label: 'exit',
+                        click(){g.process.exit()}
+                    }
+                ]
+            },
+            {
+                label: 'Mode',
+                submenu:[
+                    {
+                        label: 'order',
+                        click(){g.win.webContents.executeJavaScript('updmode(1)');}
+                    },
+                    {
+                        label: 'shuffle',
+                        click(){g.win.webContents.executeJavaScript('updmode(2)');}
+                    },
+                    {
+                        label: 'repeat once',
+                        click(){g.win.webContents.executeJavaScript('updmode(3)');}
+                    }
+                ]
+            },
+            {
+                label: 'dev',
+                click(){g.win.webContents.openDevTools()}
+            }
+        ];
+}
